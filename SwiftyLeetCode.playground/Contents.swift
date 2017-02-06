19c6cb640a50e832cbb576fb13fc4ecf073f2dcb
Peng <guoleii@gmail.com>
2017-02-06 11:33:37 +0800

    219. Contains Duplicate II

--- SwiftyLeetCode.playground/Contents.swift
+++ SwiftyLeetCode.playground/Contents.swift
@@ -2,14 +2,16 @@
 
 import UIKit
 
+class Solution {}
+
 /**
  * 217. Contains Duplicate
  * https://leetcode.com/problems/contains-duplicate/
  */
 
-class Solution {
+extension Solution {
     class func containsDuplicate(nums: [Int]?) -> Bool {
-        guard let nums = nums, nums.count != 0, nums.count != 1 else { return false }
+        guard let nums = nums, nums.count > 1 else { return false }
         
         return nums.count > Set(nums).count
     }
@@ -25,3 +27,36 @@ Solution.containsDuplicate(nums: nums1)
 
 let nums2 = [3, 9, 32, 16, 0, 125, 1457, 83]
 Solution.containsDuplicate(nums: nums2)
+
+/**
+ * 219. Contains Duplicate
+ * https://leetcode.com/problems/contains-duplicate-ii/
+ */
+
+extension Solution {
+    class func containsDuplicateII(nums: [Int]?,  _ k: Int) -> Bool {
+        guard let nums = nums, nums.count > 1 else { return false }
+        
+        var dict = [Int: Int]()
+        
+        for i in 0 ..< nums.count {
+            guard let index = dict[nums[i]], i - index <= k else {
+                dict[nums[i]] = i
+                continue
+            }
+            
+            return true
+        }
+        
+        return false
+    }
+}
+
+/*:
+ ### Usage
+ */
+
+Solution.containsDuplicateII(nums: nums1, 3)
+Solution.containsDuplicateII(nums: nums1, 5)
+Solution.containsDuplicateII(nums: nums2, 2)
+
