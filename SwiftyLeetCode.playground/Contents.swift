b3a7aab11146e32c4ed39c2eb46d6091e9be0e3b
Peng <guoleii@gmail.com>
2017-02-13 10:30:28 +0800

    167. Two Sum II - Input arary is sorted

--- SwiftyLeetCode.playground/Contents.swift
+++ SwiftyLeetCode.playground/Contents.swift
@@ -12,21 +12,21 @@ class Solution {}
  */
 
 extension Solution {
-    class func twoSum(nums: [Int], _ target: Int) -> [Int] {
-        
-        guard nums.count > 1 else { return [] }
-        
-        var map = [Int:Int]()
-        
-        for i in 0..<nums.count {
+    class func twoSum(nums: [Int]?, _ target: Int) -> [Int]? {
+
+        guard let nums = nums, nums.count > 1 else { return nil }
+
+        var map = [Int: Int]()
+
+        for i in 0 ..< nums.count {
             if let value = map[nums[i]] {
                 return [value + 1, i + 1]
             }
-            
+
             map[target - nums[i]] = i
         }
-        
-        return []
+
+        return nil
     }
 }
 
@@ -35,9 +35,46 @@ extension Solution {
  */
 
 let twoSumNums = [2, 7, 11, 15]
-let target = 9
+let twoSumTarget = 9
 
-Solution.twoSum(nums: twoSumNums, target)
+Solution.twoSum(nums: twoSumNums, twoSumTarget)
+
+/**
+ * 167. Two Sum II - Input arary is sorted
+ * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+ */
+
+extension Solution {
+    class func twoSumII(nums: [Int]?, _ target: Int) -> [Int]? {
+        guard let nums = nums, nums.count > 1 else { return nil }
+
+        var start = 0
+        var end = nums.count - 1
+
+        while start < end {
+            if nums[start] + nums[end] == target {
+                return [start + 1, end + 1]
+            }
+
+            if nums[start] + nums[end] < target {
+                start += 1
+            } else {
+                end -= 1
+            }
+        }
+
+        return nil
+    }
+}
+
+/*:
+ ### Usage
+ */
+
+let twoSumIINums = [2, 7, 11, 15]
+let twoSumIITarget = 9
+
+Solution.twoSum(nums: twoSumIINums, twoSumIITarget)
 
 /**
  * 217. Contains Duplicate
@@ -47,10 +84,10 @@ Solution.twoSum(nums: twoSumNums, target)
 extension Solution {
     class func containsDuplicate(nums: [Int]?) -> Bool {
         guard let nums = nums, nums.count > 1 else { return false }
-        
+
         return nums.count > Set(nums).count
     }
-    
+
 }
 
 /*:
@@ -69,20 +106,20 @@ Solution.containsDuplicate(nums: nums2)
  */
 
 extension Solution {
-    class func containsDuplicateII(nums: [Int]?,  _ k: Int) -> Bool {
+    class func containsDuplicateII(nums: [Int]?, _ k: Int) -> Bool {
         guard let nums = nums, nums.count > 1 else { return false }
-        
+
         var dict = [Int: Int]()
-        
+
         for i in 0 ..< nums.count {
             guard let index = dict[nums[i]], i - index <= k else {
                 dict[nums[i]] = i
                 continue
             }
-            
+
             return true
         }
-        
+
         return false
     }
 }
@@ -94,4 +131,3 @@ extension Solution {
 Solution.containsDuplicateII(nums: nums1, 3)
 Solution.containsDuplicateII(nums: nums1, 5)
 Solution.containsDuplicateII(nums: nums2, 2)
-
